782dba5d350877377fbfd9786cf99c3b9ae5db10
Alexis Ferrand <alferran@z1r3p2.42lyon.fr>
2020-09-26 19:28:13 +0200

    avancement fct cat

--- ex00/ft_catnumber.c
+++ ex00/ft_catnumber.c
@@ -6,27 +6,23 @@
 /*   By: lwiller <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/09/26 15:42:22 by lwiller           #+#    #+#             */
-/*   Updated: 2020/09/26 16:30:29 by lwiller          ###   ########lyon.fr   */
+/*   Updated: 2020/09/26 19:27:25 by alferran         ###   ########lyon.fr   */
 /*                                                                            */
 /* ************************************************************************** */
 
-int		ft_strcmp(char *to_find,char *model)
+int	ft_nbcmp(char *s1, char *s2)
 {
 	int i;
 
 	i = 0;
-	while (to_find[i])
+	while (s1[i] || (s2[i] != ' ' && s2[i] != ':'))
 	{
-		if (to_find[i] != model[i])
-			return (0);
+		if (s1[i] != s2[i])
+			return (s1[i] - s2[i]);
 		i++;
-		if (!to_find[i] && model[i] == ' ')
-			return (1);
 	}
 	return (0);
-
-
-}	
+}
 
 int		ft_strlen(char *str)
 {
@@ -38,17 +34,63 @@ int		ft_strlen(char *str)
 	return (i);
 }
 
-char	*ft_catnumber(char *str, char **tab)
+int		check_addon(char *nbr, char **dico)
 {
-	int ln_str;
-	int i;
-	char *result;
+	int	j;
 
-	ln_str = ft_strlen(str);
-		
+	j = 0;
+	while (dico[j])
+	{
+		if (!ft_nbcmp(nbr, dico[j]))
+		{
+			ft_aff(nbr, dico);
+			return (1);
+		}
+		j++;
+	}
+	return (0);
+}
+
+void	ft_catnumber(char *nbr, char **dico)
+{
+	int		ln_str;
+	int		i;
+	char	*cut;
+	int		j;
+
+	ln_str = ft_strlen(nbr);
+	while (nbr[i])
+	{
+		j = 0;
+		if (!check_addon(&nbr[i], dico))
+		{
+			if (!(cut = (char *)malloc(sizeof(char) * (ln_str + 1))))
+				return ;
+			cut[0] = nbr[i];
+			j = 1;
+			while (j < ft_strlen(&nbr[i]))
+			{
+				cut[j] = '0';
+				j++;
+			}
+			cut[j] = '\0';
+			j = 0;
+			if (!check_addon(cut, dico))
+			{
+				cut[0] = 1;
+				ft_aff(nbr[i], dico);
+				ft_aff(cut, dico);
+			}
+			free(cut);
+			i++;
+		}
+		else
+			return ;
+	}
+
 }
 
 int main(int argc, char **argv)
 {
-	
+
 }
