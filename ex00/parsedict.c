0c07cb7792c34081e1529e002218f338845d4207
Adrien Stucky <astucky@z1r3p4.42lyon.fr>
2020-09-27 18:00:10 +0200

    norme parsedict

--- ex00/parsedict.c
+++ ex00/parsedict.c
@@ -6,65 +6,53 @@
 /*   By: lwiller <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/09/27 17:45:53 by lwiller           #+#    #+#             */
-/*   Updated: 2020/09/27 17:46:15 by lwiller          ###   ########lyon.fr   */
+/*   Updated: 2020/09/27 17:58:10 by astucky          ###   ########lyon.fr   */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "rush02.h"
 
-int		ft_parsedict_2(char **dico, char *ref_i, int i)
+int		ft_parsedict_2(char **dico, char *ref_i)
 {
 	int j;
+
 	j = 0;
-	while (ft_linecmp(ref[i], dico[j]) && dico[j])
+	while (dico[j] && ft_linecmp(ref_i, dico[j]))
 		j++;
 	if (!dico[j])
-	{
-		i = 0;
-		while (ref[i])
-		{
-			free(ref[i]);
-			i++;
-		}
-		free(ref);
 		return (0);
+	return (1);
+}
+
+void	freefile(char **file)
+{
+	int i;
+
+	i = 0;
+	while (file[i])
+	{
+		free(file[i]);
+		i++;
 	}
-	return (i);
+	free(file);
 }
 
 int		ft_parsedict(char **dico)
 {
 	int		i;
-	int		j;
 	char	**ref;
 
 	ref = ft_readfile("ref.dict");
 	i = 0;
 	while (ref[i])
 	{
-	/*	j = 0;
-		while (ft_linecmp(ref[i], dico[j]) && dico[j])
-			j++;
-		if (!dico[j])
+		if (!ft_parsedict_2(dico, ref[i]))
 		{
-			i = 0;
-			while (ref[i])
-			{
-				free(ref[i]);
-				i++;
-			}
-			free(ref);
+			freefile(ref);
 			return (0);
-		}*/
-		i = ft_parsedict_2(dico, ref[i], i);
+		}
 		i++;
 	}
-	i = 0;
-	while (ref[i])
-	{
-		free(ref[i]);
-		i++;
-	}
-	free(ref);
+	freefile(ref);
 	return (1);
 }
