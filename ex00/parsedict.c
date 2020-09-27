32396ea7dadd8d28fc963617d9b834f95c6e7dfd
Adrien Stucky <astucky@z1r3p4.42lyon.fr>
2020-09-27 15:42:54 +0200

    finished parsing

---
+++ ex00/parsedict.c
@@ -0,0 +1,49 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   parsedict.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: astucky <marvin@42.fr>                     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2020/09/27 14:55:45 by astucky           #+#    #+#             */
+/*   Updated: 2020/09/27 15:42:35 by astucky          ###   ########lyon.fr   */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "rush02.h"
+
+int		ft_parsedict(char **dico)
+{
+	int		i;
+	int		j;
+	char	**ref;
+
+	ref = ft_readfile("ref.dict");
+	i = 0;
+	while (ref[i])
+	{
+		j = 0;
+		while (ft_linecmp(ref[i], dico[j]) && dico[j])
+			j++;
+		if (!dico[j])
+		{
+			i = 0;
+			while (ref[i])
+			{
+				free(ref[i]);
+				i++;
+			}
+			free(ref);
+			return (0);
+		}
+		i++;
+	}
+	i = 0;
+	while (ref[i])
+	{
+		free(ref[i]);
+		i++;
+	}
+	free(ref);
+	return (1);
+}
