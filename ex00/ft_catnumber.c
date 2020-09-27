5ce7cacc280d74471a155dbe861933f9c82d9ac0
Laurent Willer <lwiller@z1r4p3.42lyon.fr>
2020-09-27 18:20:38 +0200

    catnb a la norme et strlen

--- ex00/ft_catnumber.c
+++ ex00/ft_catnumber.c
@@ -6,7 +6,7 @@
 /*   By: lwiller <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/09/26 15:42:22 by lwiller           #+#    #+#             */
-/*   Updated: 2020/09/27 16:42:50 by astucky          ###   ########lyon.fr   */
+/*   Updated: 2020/09/27 18:18:35 by lwiller          ###   ########lyon.fr   */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -24,16 +24,6 @@ int		ft_nbcmp(char *s1, char *s2)
 		i++;
 	}
 	return (0);
-}
-
-int		ft_strlen(char *str)
-{
-	int i;
-
-	i = 0;
-	while (str[i])
-		i++;
-	return (i);
 }
 
 int		check_addon(char *nbr, char **dico, int i)
@@ -71,33 +61,39 @@ void	for_aff(char *div, char nbr, char *cut, char **dico)
 	write(1, " ", 1);
 }
 
+void	ft_catnumber_2(char *nbr, char **dico, int i)
+{
+	int		j;
+	char	*cut;
+	char	div[2];
+
+	if (nbr[i] != '0')
+	{
+		if (!check_addon(&nbr[i], dico, 0))
+		{
+			if (!(cut = malloc(sizeof(char) * (ft_strlen(nbr) + 1))))
+				return ;
+			cut[0] = nbr[i];
+			j = 0;
+			while (++j < ft_strlen(&nbr[i]))
+				cut[j] = '0';
+			cut[j] = '\0';
+			if (!check_addon(cut, dico, 1))
+				for_aff(div, nbr[i], cut, dico);
+			free(cut);
+		}
+		else
+			return ;
+	}
+}
+
 void	ft_catnumber(char *nbr, char **dico)
 {
 	int		i;
-	char	*cut;
-	int		j;
-	char	div[2];
 
 	i = -1;
 	while (nbr[++i])
 	{
-		if (nbr[i] != '0')
-		{
-			if (!check_addon(&nbr[i], dico, 0))
-			{
-				if (!(cut = malloc(sizeof(char) * (ft_strlen(nbr) + 1))))
-					return ;
-				cut[0] = nbr[i];
-				j = 0;
-				while (++j < ft_strlen(&nbr[i]))
-					cut[j] = '0';
-				cut[j] = '\0';
-				if (!check_addon(cut, dico, 1))
-					for_aff(div, nbr[i], cut, dico);
-				free(cut);
-			}
-			else
-				return ;
-		}
+		ft_catnumber_2(nbr, dico, i);
 	}
 }
