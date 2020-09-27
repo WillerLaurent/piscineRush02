cd99fb5a3b79eeb51dcd1f754fa8ea7349e3f671
Laurent Willer <lwiller@z1r4p3.42lyon.fr>
2020-09-27 15:09:20 +0200

    ft_aff a la norme

--- ex00/ft_aff.c
+++ ex00/ft_aff.c
@@ -6,47 +6,67 @@
 /*   By: alferran <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/09/26 19:29:57 by alferran          #+#    #+#             */
-/*   Updated: 2020/09/27 14:15:23 by astucky          ###   ########lyon.fr   */
+/*   Updated: 2020/09/27 15:07:10 by lwiller          ###   ########lyon.fr   */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "rush02.h"
 
-void	ft_aff(char *nbr, char **dico)
+void	ft_aff_letters(char *str)
 {
-	int i;
 	int j;
 	int len;
 
+	j = 0;
+	len = ft_strlen(str);
+	len--;
+	while (str[len] == ' ')
+		len--;
+	while (str[j] >= '0' && str[j] <= '9')
+		j++;
+	while (str[j] == ' ')
+		j++;
+	if (str[j] == ':')
+		j++;
+	while (str[j] == ' ')
+		j++;
+	write(1, &str[j], len - j + 1);
+	return ;
+}
+
+void	ft_aff(char *nbr, char **dico)
+{
+	int i;
+
 	i = 0;
-	while(dico[i])
+	while (dico[i])
 	{
-		if (!ft_nbcmp(nbr,dico[i]))
-		{
-			j = 0;
-			len = ft_strlen(dico[i]);
-			len--;
-			while(dico[i][len] == ' ')
-				len--;
-			while (dico[i][j] >= '0' && dico[i][j] <= '9')
-				j++;
-			while (dico[i][j] == ' ')
-				j++;
-			if (dico[i][j] == ':')
-				j++;
-			while (dico[i][j] == ' ')
-				j++;
-			write(1, &dico[i][j], len - j + 1);
-			return ;
-		}
+		if (!ft_nbcmp(nbr, dico[i]))
+			ft_aff_letters(dico[i]);
 		i++;
 	}
+}
+
+void	ft_aff_sep(int nb_zeros, char *sep, char **dico)
+{
+	int j;
+
+	j = 1;
+	sep[0] = '1';
+	while (j < nb_zeros + 1)
+	{
+		sep[j] = '0';
+		j++;
+	}
+	sep[j] = '\0';
+	write(1, " ", 1);
+	ft_aff(sep, dico);
+	write(1, " ", 1);
 }
 
 void	ft_aff_all(char **tab, char **dico, int size)
 {
 	int		i;
-	int		j;
 	int		nb_zeros;
 	char	*sep;
 
@@ -61,21 +81,10 @@ void	ft_aff_all(char **tab, char **dico, int size)
 				nb_zeros = 3 * (size - i - 1);
 				if (!(sep = (char *)malloc(sizeof(char) * (nb_zeros + 2))))
 					return ;
-				j = 1;
-				sep[0] = '1';
-				while (j < nb_zeros + 1)
-				{
-					sep[j] = '0';
-					j++;
-				}
-				sep[j] = '\0';
-				write(1, " ", 1);
-				ft_aff(sep, dico);
-				write(1, " ", 1);
+				ft_aff_sep(nb_zeros, sep, dico);
 				free(sep);
 			}
 		}
 		i++;
 	}
-
 }
